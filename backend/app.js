455a35ab37ebe55dfc34a873574352202aecc72e
jrgaragon <jrg.aragon@gmail.com>
2022-06-14 20:13:04 -0500

    format

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 const instagramService = require("./services/instagramService");
 const openConnection = require("./services/dal");
 const app = express();
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: "50mb" }));
 console.log("******************************************START******************************************");
 
 require("./routes/api/site")(app);
